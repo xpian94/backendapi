ba73d315f0a283dace7ac13b402aa15893775bca
Cristian <cristian.mello@hotmail.com.br>
2024-08-14 09:53:42 -0300

    refact: TodoServiceTest boilerplate

--- src/test/java/org/example/TodoServiceTest.java
+++ src/test/java/org/example/TodoServiceTest.java
@@ -1,14 +1,9 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 public class TodoServiceTest {
-    @InjectMocks
-    private TodoService service;
+    private final TodoService service = new TodoService();
 
     @Test
     void nothing() {
