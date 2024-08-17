f5640376c222dd7ff0b6e01c0cd105c5bbe17661
Cristian <cristian.mello@hotmail.com.br>
2024-08-17 08:49:07 -0300

    feat: TodoRepository IT

--- src/main/java/org/example/TodoEntity.java
+++ src/main/java/org/example/TodoEntity.java
@@ -1,10 +1,20 @@
 package org.example;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    public void setTitle(String title) {
+    private String title;
 
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public Long getId() {
@@ -13,5 +23,9 @@ public class TodoEntity {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }
---
+++ src/test/java/org/example/TodoRepositoryIntegrationTest.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class TodoRepositoryIntegrationTest {
+    @Autowired
+    private TodoRepository repo;
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Test
+    void givenNewTodoEntity_whenPersist_thenIdIsIncremented() {
+        var todoEntity = new TodoEntity();
+        var saved = repo.save(todoEntity);
+
+        assertThat(saved.getId()).isEqualTo(1L);
+
+        todoEntity = new TodoEntity();
+        saved = repo.save(todoEntity);
+
+        assertThat(saved.getId()).isEqualTo(2L);
+    }
+
+    @Test
+    void giveNewTodoEntity_whenPersist_thenSuccess() {
+        var todoEntity = new TodoEntity();
+
+        var saved = repo.save(todoEntity);
+        var found = entityManager.find(TodoEntity.class, saved.getId());
+
+        assertThat(found).isEqualTo(todoEntity);
+    }
+}
