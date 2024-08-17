b46545a78fdbf50604183d63ad9c92ed7c2d4ed4
Cristian <cristian.mello@hotmail.com.br>
2024-08-17 16:33:38 -0300

    feat: adding request all todos (basic)

--- src/main/java/xpian94/todo/TodoService.java
+++ src/main/java/xpian94/todo/TodoService.java
@@ -8,6 +8,8 @@ public class TodoService {
     @Autowired
     private TodoRepository repo;
 
+    private static int counter = 0;
+
     TodoResponse create(TodoRequest todoRequest) {
         var entity = TodoEntity.builder()
             .title(todoRequest.getTitle())
@@ -17,6 +19,17 @@ public class TodoService {
 
         var response = new TodoResponse();
         response.setId(String.valueOf(saved.getUuid()));
+
+        counter++;
+
+        return response;
+    }
+
+    public TodoResponseAll requestAll() {
+        var response = new TodoResponseAll();
+
+        repo.findAll()
+            .forEach(todoEntity -> response.getTodos().add(new TodoResponse()));
 
         return response;
     }
--- src/test/java/xpian94/todo/TodoIntegrationTest.java
+++ src/test/java/xpian94/todo/TodoIntegrationTest.java
@@ -2,11 +2,9 @@ package xpian94.todo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import xpian94.IntegrationTestBase;
@@ -15,8 +13,6 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Transactional
-@AutoConfigureTestEntityManager
 public class TodoIntegrationTest extends IntegrationTestBase {
     @Autowired
     private TestRestTemplate restTemplate;
@@ -24,10 +20,14 @@ public class TodoIntegrationTest extends IntegrationTestBase {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
-    private TestEntityManager testEntityManager;
+    private TodoRepository repository;
 
-    @Test
-    void givenRequestBody_whenCreate_thenSuccess() throws JsonProcessingException {
+    @BeforeEach
+    void setup() {
+        repository.deleteAll();
+    }
+
+    private ResponseEntity<String> requestTodoCreation() {
         var headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -40,7 +40,30 @@ public class TodoIntegrationTest extends IntegrationTestBase {
         var request = new HttpEntity<>(body, headers);
 
         var url = "http://localhost:%d%s".formatted(port, "/todo");
-        var response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+
+        return restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+    }
+
+    private ResponseEntity<String> requestAllTodos() {
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        var request = new HttpEntity<>("", headers);
+
+        var url = "http://localhost:%d%s".formatted(port, "/todo");
+        return restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+    }
+
+    private void verifyingTodosListSize(ResponseEntity<String> response, int expectedSize) throws JsonProcessingException {
+        var root = objectMapper.readTree(response.getBody());
+        var todos = root.findPath("todos");
+
+        assertThat(todos.size()).isEqualTo(expectedSize);
+    }
+
+    @Test
+    void givenRequestBody_whenCreate_thenSuccess() throws JsonProcessingException {
+        var response = requestTodoCreation();
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
@@ -51,12 +74,23 @@ public class TodoIntegrationTest extends IntegrationTestBase {
         assertThat(root).isNotNull();
         assertThat(idNodePath).isNotNull();
 
-        var entityManager = testEntityManager.getEntityManager();
+        var found = repository.findByUuid(UUID.fromString(id));
 
-        var entity = (TodoEntity) entityManager
-            .createQuery("SELECT t FROM todo t WHERE t.uuid = :uuid")
-            .setParameter("uuid", UUID.fromString(id)).getSingleResult();
+        assertThat(found).isNotNull();
+    }
 
-        assertThat(entity).isNotNull();
+    @Test
+    void givenCreatedTodo_whenRequestListAll_thenSuccess() throws JsonProcessingException {
+        var response = requestAllTodos();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        verifyingTodosListSize(response, 0);
+
+        requestTodoCreation();
+
+        response = requestAllTodos();
+
+        verifyingTodosListSize(response, 1);
     }
 }
